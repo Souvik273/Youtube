4885b84d0040352a603f88d25665cc86fd7f75c6
Souvik273 <souvikgoswami464@gmail.com>
2025-01-16 01:39:08 +0530

    utils code updated

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,12 @@
 require('dotenv').config()
 
 const connectDB = require('../db/db.js')
+const app = require('./app.js')
 
 connectDB()
-    .then(() => console.log('Database connection established'))
+    .then(() => {
+        app.listen(process.env.PORT || 8000 , ()=>{
+            console.log(`DB connection establish !!! \n server is running on : http://localhost:${process.env.PORT || 8000}`)
+        })
+    })
     .catch((err) => console.error('Failed to connect:', err.message));
