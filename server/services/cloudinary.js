b442765e5fe616da390aab0da25e92a3aeee9ce4
Souvik273 <souvikgoswami464@gmail.com>
2025-01-16 13:50:10 +0530

    multer and cloudinary setup done for file uploads

---
+++ server/services/cloudinary.js
@@ -0,0 +1,30 @@
+const { v2 } = require('cloudinary')
+const fs = require('fs')
+
+v2.config({
+    cloud_name: CLOUDINARY_CLOUD_NAME,
+    api_key: CLOUDINARY_API_KEY,
+    api_secret: CLOUDINARY_SECRET
+})
+
+const uploadOnCloudinary = async (localfilepath) => {
+    try {
+        if (!localfilepath) return null
+
+        // upload file on cloudinary
+        const response = await cloudinary.uploader.upload(localfilepath, {
+            resource_type: 'auto'
+        })
+
+        // file has been uploaded successfully
+        console.log(`File is uploaded successfully : ${response.url}`)
+
+        return response
+    } catch {
+        // remove locally saved temopary file as the upload operation got failed
+        fs.unlinkSync(localfilepath)
+        return null
+    }
+}
+
+module.exports = uploadOnCloudinary
