532c14b83fd79991f816c351045d4765f1cecf67
Souvik273 <souvikgoswami464@gmail.com>
2025-01-17 15:51:22 +0530

    user controller updated

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -24,7 +24,7 @@ const registerUser = asyncHandler(async (req, res) => {
         throw new ApiError(400, "All fields are required")
     }
 
-    const existedUser = User.findOne({
+    const existedUser = await User.findOne({
         $or: [{ username }, { email }]
     })
 
@@ -34,7 +34,12 @@ const registerUser = asyncHandler(async (req, res) => {
 
     const avatarLocalPath = req.files?.avatar[0]?.path
 
-    const coverIageLocalPath = req.files?.coverImage[0]?.path
+    // const coverIageLocalPath = req.files?.coverImage[0]?.path
+
+    let coverIageLocalPath;
+    if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+        coverIageLocalPath = req.files.coverImage[0].path
+    }
 
     if (!avatarLocalPath) {
         throw new ApiError(404, "Avatar required")
