d129cb824b293808d129808460c255ce77b13c08
Souvik273 <souvikgoswami464@gmail.com>
2025-01-22 15:07:02 +0530

    authentication middleware setup done

---
+++ server/middlewares/auth.middleware.js
@@ -0,0 +1,28 @@
+const { jwt } = require("jsonwebtoken");
+const { ApiError } = require("../utils/ApiError");
+const asyncHandler = require("../utils/asyncHandler.js");
+const User = require("../models/user.model");
+
+const verifyJWT = asyncHandler(async(req,_,next)=>{
+    try {
+        const token = await req.cookie?.accessToken || req.headers("Authorisation")?.replace("Bearer","")
+        if(!token){
+            throw new ApiError(401,"Unauthorise access")
+        }
+    
+        const decoded = jwt.verify(token,process.env.ACCESS_TOKEN_SECRET)
+    
+        const user = await User.findOne(decoded?._id).select("-password refreshToken")
+        
+        if(!user){
+            throw new ApiError(401,"Invalid user ")
+        }
+    
+        req.user = user
+        next()
+    } catch (error) {
+        throw new ApiError(401,"Unverified user")
+    }
+})
+
+module.exports = verifyJWT
