5f860bded801fb237eb5d6c0241c52a3909e1304
Souvik273 <souvikgoswami464@gmail.com>
2025-01-25 16:29:47 +0530

    multer comfig updated

--- server/middlewares/multer.middleware.js
+++ server/middlewares/multer.middleware.js
@@ -1,15 +1,21 @@
-const multer = require('multer')
+const multer = require('multer');
+const path = require('path');
+const fs = require('fs');
+
+const tempDir = path.join(__dirname, '..', 'public', 'temp');
+if (!fs.existsSync(tempDir)) {
+    fs.mkdirSync(tempDir, { recursive: true });
+}
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, './public/temp')
+        cb(null, tempDir);
     },
     filename: function (req, file, cb) {
-        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-        cb(null, file.fieldname + '-' + uniqueSuffix)
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+        cb(null, file.fieldname + '-' + uniqueSuffix);
     }
-})
+});
 
-const upload = multer({ storage })
-
-module.exports = { upload }
+const upload = multer({ storage });
+module.exports = { upload };
